e0bd7124db188e9fc807037bda75a872de190e19
Jonsen22 <gabriel.jansen222@gmail.com>
2021-08-12 14:05:51 -0300

    add controllerTest

---
+++ src/test/java/PM/G10/ControllerTest.java
@@ -0,0 +1,42 @@
+package PM.G10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.Console;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import PM.G10.util.JavalinApp;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+
+public class ControllerTest {
+	private static JavalinApp app = new JavalinApp();
+	
+	@BeforeAll
+	static void init() {
+		app.start(7010);
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		app.stop();
+	}
+	
+	@Test
+	void getEchoTest() {
+		HttpResponse response = Unirest.get("http://localhost:7010/xd").asString();
+		assertEquals(200, response.getStatus());
+		assertEquals("xd xd", response.getBody());
+	}
+	
+	@Test
+	void getRootTest() {
+		HttpResponse response = Unirest.get("http://localhost:7010/").asString();
+		assertEquals(200, response.getStatus());
+		assertEquals("Isto é um eco, digite algo a mais no caminho",response.getBody());
+	}
+}
